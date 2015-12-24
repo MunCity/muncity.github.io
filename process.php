c2c5f6c1878c9bc1ad1fad24b04535dfae0e9e2f
MunCity <muncityentertainment@gmail.com>
2015-12-23 21:49:39 -0800

    Update process.php

--- process.php
+++ process.php
@@ -73,5 +73,5 @@ if(empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUE
     header('location: ' . $_SERVER['HTTP_REFERER']);
 }
 }
-                            
+ ?>                           
                             
